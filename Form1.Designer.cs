3f5e1571de0e5375a64135c6e762d2a8e8d26022
ferhatYuksel13 <sehinsah1333@gmail.com>
2024-12-10 18:52:00 +0300

    döviz çevirme programı

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace DovizCevirici
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Gerekli bileşenler
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Kullanıcı kontrolündeki kullanılan tüm kaynakları temizler.
+        /// </summary>
+        /// <param name="disposing">Managed kaynakların temizlenmesi gerektiğini belirten bir değer.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Tasarım için gerekli olan yöntem. 
+        /// Bu yöntem, Windows Form Designer tarafından çağrılır.
+        /// Kodunuzu burada değiştirmeyin. 
+        /// Bu kodu, Windows Form Designer tarafından eklenip, silinmesini sağlar.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbFromCurrency = new System.Windows.Forms.ComboBox();
+            this.cmbToCurrency = new System.Windows.Forms.ComboBox();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.btnUpdateRates = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblLastUpdated = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbFromCurrency
+            // 
+            this.cmbFromCurrency.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.cmbFromCurrency.FormattingEnabled = true;
+            this.cmbFromCurrency.Location = new System.Drawing.Point(50, 50);
+            this.cmbFromCurrency.Name = "cmbFromCurrency";
+            this.cmbFromCurrency.Size = new System.Drawing.Size(121, 24);
+            this.cmbFromCurrency.TabIndex = 0;
+            // 
+            // cmbToCurrency
+            // 
+            this.cmbToCurrency.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.cmbToCurrency.FormattingEnabled = true;
+            this.cmbToCurrency.Location = new System.Drawing.Point(200, 50);
+            this.cmbToCurrency.Name = "cmbToCurrency";
+            this.cmbToCurrency.Size = new System.Drawing.Size(121, 24);
+            this.cmbToCurrency.TabIndex = 1;
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txtAmount.Location = new System.Drawing.Point(50, 100);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(100, 22);
+            this.txtAmount.TabIndex = 2;
+            // 
+            // btnUpdateRates
+            // 
+            this.btnUpdateRates.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnUpdateRates.Location = new System.Drawing.Point(50, 150);
+            this.btnUpdateRates.Name = "btnUpdateRates";
+            this.btnUpdateRates.Size = new System.Drawing.Size(121, 30);
+            this.btnUpdateRates.TabIndex = 3;
+            this.btnUpdateRates.Text = " Güncelle";
+            this.btnUpdateRates.UseVisualStyleBackColor = false;
+            this.btnUpdateRates.Click += new System.EventHandler(this.btnUpdateRates_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnConvert.Location = new System.Drawing.Point(200, 150);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(121, 30);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "Çevir";
+            this.btnConvert.UseVisualStyleBackColor = false;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.lblResult.Location = new System.Drawing.Point(50, 200);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(180, 16);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Text = "Döviz Çeviriciye Hoşgeldiniz!";
+            // 
+            // lblLastUpdated
+            // 
+            this.lblLastUpdated.AutoSize = true;
+            this.lblLastUpdated.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.lblLastUpdated.Location = new System.Drawing.Point(50, 230);
+            this.lblLastUpdated.Name = "lblLastUpdated";
+            this.lblLastUpdated.Size = new System.Drawing.Size(210, 16);
+            this.lblLastUpdated.TabIndex = 6;
+            this.lblLastUpdated.Text = "Son güncelleme: Henüz yapılmadı";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(199)))), ((int)(((byte)(55)))));
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.lblLastUpdated);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.btnUpdateRates);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.cmbToCurrency);
+            this.Controls.Add(this.cmbFromCurrency);
+            this.Name = "Form1";
+            this.Text = "Döviz Çevirici";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbFromCurrency;
+        private System.Windows.Forms.ComboBox cmbToCurrency;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Button btnUpdateRates;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblLastUpdated;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.IO;
+using System.Net;
+
+namespace DovizCevirici
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Data Source=LEGEND;Initial Catalog=DovizDB;Integrated Security=True;Encrypt=False;";
+        private string logFilePath = "error_log.txt"; // Hata kaydının yazılacağı dosya yolu
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            LoadCurrencyCodes(); // Döviz kodlarını yükle
+            lblResult.Text = "Döviz Çeviriciye Hoşgeldiniz!";
+            lblLastUpdated.Text = "Son güncelleme: Henüz yapılmadı";
+            await UpdateExchangeRates(); // Döviz oranlarını güncelle
+        }
+
+        private async void btnUpdateRates_Click(object sender, EventArgs e)
+        {
+            await UpdateExchangeRates(); // Döviz oranlarını güncelle
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            ConvertCurrency(); // Döviz çevirme işlemini yap
+        }
+
+        // Döviz kodlarını veritabanından yükle ve ComboBox'ları doldur
+        private void LoadCurrencyCodes()
+        {
+            try
+            {
+                cmbFromCurrency.Items.Clear();
+                cmbToCurrency.Items.Clear();
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT currency_code FROM exchange_rates"; // exchange_rates tablosundaki döviz kodlarını al
+                    using (SqlCommand cmd = new SqlCommand(query, connection))
+                    {
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            string currencyCode = reader["currency_code"].ToString();
+                            cmbFromCurrency.Items.Add(currencyCode);
+                            cmbToCurrency.Items.Add(currencyCode);
+                        }
+                    }
+                }
+
+                if (cmbFromCurrency.Items.Count > 0)
+                {
+                    cmbFromCurrency.SelectedIndex = 0;
+                    cmbToCurrency.SelectedIndex = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Veritabanı hatası: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Döviz oranlarını TCMB API'sinden al ve veritabanını güncelle
+        private async Task UpdateExchangeRates()
+        {
+            try
+            {
+                // Loading spinner'ı göster
+                ShowLoadingSpinner(true);
+
+                string url = "https://www.tcmb.gov.tr/kurlar/today.xml"; // TCMB döviz verisi XML URL'si
+                HttpClient client = new HttpClient();
+                string xmlData = await client.GetStringAsync(url); // XML verisini indir
+                XDocument xdoc = XDocument.Parse(xmlData); // XML verisini parse et
+
+                var currencies = xdoc.Descendants("Currency")
+                                     .Where(c => c.Element("ForexBuying") != null) // ForexBuying elementini kontrol et
+                                     .Select(c => new
+                                     {
+                                         Code = c.Attribute("CurrencyCode").Value, // Döviz kodu
+                                         Rate = decimal.Parse(c.Element("ForexBuying").Value) // Döviz kuru
+                                     }).ToList();
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    foreach (var currency in currencies)
+                    {
+                        string query = "SELECT rate FROM exchange_rates WHERE currency_code = @currency_code"; // Veritabanında döviz kodu araması
+                        using (SqlCommand cmd = new SqlCommand(query, connection))
+                        {
+                            cmd.Parameters.AddWithValue("@currency_code", currency.Code);
+                            var currentRate = cmd.ExecuteScalar();
+
+                            // Eğer döviz oranı değişmişse, güncelleme yap
+                            if (currentRate == null)
+                            {
+                                // Döviz kodu yoksa yeni ekle
+                                string insertQuery = "INSERT INTO exchange_rates (currency_code, rate, updated_at) VALUES (@currency_code, @rate, GETDATE())";
+                                using (SqlCommand insertCmd = new SqlCommand(insertQuery, connection))
+                                {
+                                    insertCmd.Parameters.AddWithValue("@currency_code", currency.Code);
+                                    insertCmd.Parameters.AddWithValue("@rate", currency.Rate);
+                                    insertCmd.ExecuteNonQuery();
+                                }
+                            }
+                            else if ((decimal)currentRate != currency.Rate)
+                            {
+                                string updateQuery = "UPDATE exchange_rates SET rate = @rate, updated_at = GETDATE() WHERE currency_code = @currency_code";
+                                using (SqlCommand updateCmd = new SqlCommand(updateQuery, connection))
+                                {
+                                    updateCmd.Parameters.AddWithValue("@rate", currency.Rate);
+                                    updateCmd.Parameters.AddWithValue("@currency_code", currency.Code);
+                                    updateCmd.ExecuteNonQuery();
+                                }
+                            }
+                        }
+                    }
+                }
+
+                lblLastUpdated.Text = $"Son güncelleme: {DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss")}";
+                MessageBox.Show("Döviz oranları başarıyla güncellendi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (WebException webEx)
+            {
+                LogError(webEx.Message);
+                MessageBox.Show($"API hatası: {webEx.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                LogError(ex.Message);
+                MessageBox.Show($"Hata: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                // Loading spinner'ı gizle
+                ShowLoadingSpinner(false);
+            }
+        }
+
+        // Döviz çevirme işlemini yap
+        private void ConvertCurrency()
+        {
+            if (string.IsNullOrEmpty(txtAmount.Text) || !decimal.TryParse(txtAmount.Text, out decimal amount) || amount <= 0)
+            {
+                MessageBox.Show("Lütfen geçerli bir tutar girin.", "Geçersiz Tutar", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string fromCurrency = cmbFromCurrency.SelectedItem.ToString();
+            string toCurrency = cmbToCurrency.SelectedItem.ToString();
+
+            decimal fromRate, toRate;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT rate FROM exchange_rates WHERE currency_code = @currency_code";
+
+                    using (SqlCommand cmd = new SqlCommand(query, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@currency_code", fromCurrency);
+                        fromRate = (decimal)cmd.ExecuteScalar();
+
+                        cmd.Parameters["@currency_code"].Value = toCurrency;
+                        toRate = (decimal)cmd.ExecuteScalar();
+                    }
+                }
+
+                decimal convertedAmount = (amount / fromRate) * toRate;
+
+                // Döviz çevirisini düzgün formatta göster
+                lblResult.Text = $"{amount} {fromCurrency} = {convertedAmount.ToString("#,0.00")} {toCurrency}";
+                lblResult.ForeColor = System.Drawing.Color.Green;
+            }
+            catch (Exception ex)
+            {
+                LogError(ex.Message);
+                MessageBox.Show($"Çeviri hatası: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Loading spinner'ı göster/gizle
+        private void ShowLoadingSpinner(bool show)
+        {
+            // Loading spinner yok, bu metod boş bırakılabilir
+        }
+
+        // Hata loglama
+        private void LogError(string errorMessage)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(logFilePath, true))
+                {
+                    writer.WriteLine($"{DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss")}: {errorMessage}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hata kaydedilemedi: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
